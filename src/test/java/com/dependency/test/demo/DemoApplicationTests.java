eb56311176d144afca28e6866f6d8d8b939ba3cc
myh9410 <mjskks94@naver.com>
2023-11-02 18:16:24 +0900

    testcontainer 적용 테스트

--- src/test/java/com/dependency/test/demo/DemoApplicationTests.java
+++ src/test/java/com/dependency/test/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.dependency.test.demo;
 
+import org.junit.ClassRule;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
+@Testcontainers
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DemoApplicationTests {
 
+	@ClassRule
+	public MySQLContainer mysqlContainer = new MySQLContainer();
 	@Test
-	void contextLoads() {
+	void test() {
+		assertFalse(mysqlContainer.isRunning());
+		System.out.println("test end");
 	}
 
 }
