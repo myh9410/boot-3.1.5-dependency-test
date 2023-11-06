7afc390bb524855ed4bd7002463cdbddd8638dc0
myh9410 <mjskks94@naver.com>
2023-11-06 11:46:05 +0900

    oracle container 추가, 이전 테스트의 transaction 결과 확인

--- src/main/java/com/dependency/test/demo/domains/users/Users.java
+++ src/main/java/com/dependency/test/demo/domains/users/Users.java
@@ -3,12 +3,12 @@ package com.dependency.test.demo.domains.users;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users", schema = "test")
 public class Users {
--- src/test/java/com/dependency/test/demo/DemoApplicationTests.java
+++ src/test/java/com/dependency/test/demo/DemoApplicationTests.java
@@ -11,14 +11,15 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.OracleContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @Testcontainers
@@ -35,10 +36,17 @@ class DemoApplicationTests {
 
 	private static JdbcTemplate jdbcTemplate;
 
+	//todo : multidatasource를 사용해야 하는 경우의 test container 설정 및 간략한 코드 추가
+
 	@Container
 	private static MySQLContainer mysqlContainer = new MySQLContainer("mysql:8")
 			.withUsername(USER_NAME)
 			.withPassword(USER_PASSWORD);
+
+//	@Container
+//	private static OracleContainer oracleContainer = new OracleContainer("gvenzl/oracle-xe:21-slim-faststart")
+//			.withUsername(USER_NAME)
+//			.withPassword(USER_PASSWORD);
 
 	@BeforeAll
 	static void startUp() {
@@ -74,6 +82,7 @@ class DemoApplicationTests {
 	}
 
 	@Test
+	@Transactional
 	void checkInitSqlExecuted() {
 		Optional<Users> optionalUsers1 = usersRepository.findById(1L);
 		Optional<Users> optionalUsers2 = usersRepository.findById(2L);
@@ -83,6 +92,16 @@ class DemoApplicationTests {
 
 		assertTrue(optionalUsers2.isPresent());
 		assertEquals(optionalUsers2.get().getUserId(), "user2");
+
+		Users users = Users.builder().no(3L).userId("user3").build();
+//		usersRepository.saveAndFlush(users);
+	}
+
+	@Test
+	void checkTransactionResultOfCheckInitSqlExecutedFunction() {
+		//앞선 함수의 명시적인 save나 saveAndFlush의 영향을 받지 않음. dirty checking을 통해 저장된 entity도 영향 없음.
+		Optional<Users> optionalUsers = usersRepository.findById(3L);
+		assertFalse(optionalUsers.isPresent());
 	}
 
 }
